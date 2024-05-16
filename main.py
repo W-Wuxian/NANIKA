ed1d14f6f62f792d74e6930a947b5ba95f9d1748
Victor Lederer <victor.lederer@u-bordeaux.fr>
2024-05-16 15:48:38 +0200

    ext

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders import (
     DirectoryLoader, UnstructuredPDFLoader, TextLoader,
     PythonLoader, UnstructuredImageLoader,
     UnstructuredExcelLoader, UnstructuredWordDocumentLoader, UnstructuredXMLLoader,
-    UnstructuredCSVLoader, UnstructuredPowerPointLoader,
+    UnstructuredCSVLoader, UnstructuredPowerPointLoader, UnstructuredODTLoader,
     UnstructuredMarkdownLoader
 )
 from langchain_community.embeddings import OllamaEmbeddings
@@ -38,8 +38,20 @@ def routerloader(obj):
         if Fname.endswith(".xlsx") or Fname.endswith(".xls"):
             loader = UnstructuredExcelLoader(str(obj), mode="single", strategy="hi_res",
             show_progress=True, use_multithreading=True)
+        if Fname.endswith(".odt"):
+            loader = UnstructuredODTLoader(str(obj), mode="single", strategy="hi_res",
+            show_progress=True, use_multithreading=True)
         if Fname.endswith(".csv"):
             loader = UnstructuredCSVLoader(str(obj), mode="single", strategy="hi_res",
+            show_progress=True, use_multithreading=True)
+        if Fname.endswith(".pptx"):
+            loader = UnstructuredPowerPointLoader(str(obj), mode="single", strategy="hi_res",
+            show_progress=True, use_multithreading=True)
+        if Fname.endswith(".md"):
+            loader = UnstructuredMarkdownLoader(str(obj), mode="single", strategy="hi_res",
+            show_progress=True, use_multithreading=True)
+        if Fname.endswith(".org"):
+            loader = UnstructuredOrgModeLoader(str(obj), mode="single", strategy="hi_res",
             show_progress=True, use_multithreading=True)
         accumulator.extend(loader.load())
     elif os.path.isdir(obj):
@@ -91,10 +103,38 @@ def routerloader(obj):
                 loader_kwargs=abc, show_progress=True, use_multithreading=True
             )
             accumulator.extend(loader.load())
+        if any(File.endswith(".odt") for File in os.listdir(obj)):
+            abc={'mode': "single", 'strategy': "hi_res"}
+            loader = DirectoryLoader(
+                obj, glob="**/*.odt", loader_cls=UnstructuredODTLoader,
+                loader_kwargs=abc, show_progress=True, use_multithreading=True
+            )
+            accumulator.extend(loader.load())
         if any(File.endswith(".csv") for File in os.listdir(obj)):
             abc={'mode': "single", 'strategy': "hi_res"}
             loader = DirectoryLoader(
-                obj, glob="**/*.csv", loader_cls=UnstructuredExcelLoader,
+                obj, glob="**/*.csv", loader_cls=UnstructuredCSVLoader,
+                loader_kwargs=abc, show_progress=True, use_multithreading=True
+            )
+            accumulator.extend(loader.load())
+        if any(File.endswith(".pptx") for File in os.listdir(obj)):
+            abc={'mode': "single", 'strategy': "hi_res"}
+            loader = DirectoryLoader(
+                obj, glob="**/*.pptx", loader_cls=UnstructuredPowerPointLoader,
+                loader_kwargs=abc, show_progress=True, use_multithreading=True
+            )
+            accumulator.extend(loader.load())
+        if any(File.endswith(".md") for File in os.listdir(obj)):
+            abc={'mode': "single", 'strategy': "hi_res"}
+            loader = DirectoryLoader(
+                obj, glob="**/*.md", loader_cls=UnstructuredMarkdownLoader,
+                loader_kwargs=abc, show_progress=True, use_multithreading=True
+            )
+            accumulator.extend(loader.load())
+        if any(File.endswith(".org") for File in os.listdir(obj)):
+            abc={'mode': "single", 'strategy': "hi_res"}
+            loader = DirectoryLoader(
+                obj, glob="**/*.org", loader_cls=UnstructuredOrgModeLoader,
                 loader_kwargs=abc, show_progress=True, use_multithreading=True
             )
             accumulator.extend(loader.load())
@@ -106,23 +146,30 @@ def loaddata(data_path):
         documents.extend(routerloader(data))
     return documents
 
+def remove_blankline(d):
+    text = d.page_content.replace('\n\n','\n')
+    d.page_content = text
+    return d
+
 def initfromcmdlineargs():
-    PDF_ROOT_DIR    = ""
-    PDF_FOLDER_PATH = []
-    VDB_PATH        = "./default_chroma_vdb"
-    COLLECTION_NAME = "default_collection_name"
-    MODEL_NAME      = ""
-    EMBEDDING_NAME  = ""
-    REUSE_VDB       = False
+    PDF_ROOT_DIR     = ""
+    IDOC_PATH        = []
+    VDB_PATH         = "./default_chroma_vdb"
+    COLLECTION_NAME  = "default_collection_name"
+    MODEL_NAME       = ""
+    EMBEDDING_NAME   = ""
+    REUSE_VDB        = False
+    DISPLAY_DOC      = False
     argumentlist = sys.argv[1:]
-    options = "hm:e:p:v:c:r:"
+    options = "hm:e:i:v:c:r:d:"
     long_options = ["help",
                  "model_name=",
                  "embedding_name=",
-                 "pdf_path=",
+                 "inputdocs_path=",
                  "vdb_path=",
                  "collection_name=",
-                 "reuse="]
+                 "reuse=",
+                 "display_doc="]
     try:
         arguments, values = getopt.getopt(argumentlist, options, long_options)
         for currentArgument, currentValue in arguments:
@@ -132,25 +179,26 @@ def initfromcmdlineargs():
                 print ("-h or --help to get this help msg")
                 print ("-m or --model_name name of the model used, ex:phi3")
                 print ("-e or --embedding_name name of the embedding model used, ex:nomic-embed-text")
-                print ("-p or --pdf_path path list between \" \" to datas to be used for RAG")
+                print ("-i or --inputdocs_path path list between \" \" to folders or files to be used for RAG")
                 print ("-v or --vdb_path path to the directory used as a vector database")
                 print ("-c or --collection_name name of the vector database collection")
                 print ("-r or --reuse str True or False reuse vector database")
+                print("-d or --display_doc str Tur or False whether or not to display partially input documents")
                 print("Command line arguments example:")
                 print("python --model_name MyModelName --embedding_name MyEmbeddingModel \ ")
-                print("--pdf_path \"My/Path/To/folder1 My/Path/To/folder2 My/Path/To/file1\" \ ")
+                print("--inputdocs_path \"My/Path/To/folder1 My/Path/To/folder2 My/Path/To/file1\" \ ")
                 print("--collection_name MyCollectionName \ ")
-                print("--reuse False")
+                print("--reuse False --display-doc False")
                 exit()
             elif currentArgument in ("-m", "--model_name"):
                 MODEL_NAME = currentValue
             elif currentArgument in ("-e", "--embedding_name"):
                 EMBEDDING_NAME = currentValue
-            elif currentArgument in ("-p", "--pdf_path"):
+            elif currentArgument in ("-i", "--inputdocs_path"):
                 for i in currentValue.split(" "):
                     if (len(i) != 0):
                         if (os.path.isfile(i)) or ((os.path.isdir(i)) and (len(os.listdir(i)) != 0)):
-                            PDF_FOLDER_PATH.append(Path(i))
+                            IDOC_PATH.append(Path(i))
             elif currentArgument in ("-v", "--vdb_path"):
                 VDB_PATH = Path(currentValue)
             elif currentArgument in ("-c", "--collection_name"):
@@ -160,7 +208,12 @@ def initfromcmdlineargs():
                     REUSE_VDB = True
                 else:
                     REUSE_VDB = False
-        return MODEL_NAME, EMBEDDING_NAME, PDF_FOLDER_PATH, VDB_PATH, COLLECTION_NAME, REUSE_VDB
+            elif currentArgument in ("-d", "--display_doc"):
+                if currentValue.casefold() == "true":
+                    DISPLAY_DOC = True
+                else:
+                    DISPLAY_DOC = False
+        return MODEL_NAME, EMBEDDING_NAME, IDOC_PATH, VDB_PATH, COLLECTION_NAME, REUSE_VDB, DISPLAY_DOC
     except getopt.error as err:
         print (str(err))
         exit()
@@ -199,32 +252,46 @@ def create_vdb(splitted_data, embedding, vdb_path, collection_name, reuse_vdb):
 print("#-----------------------------------#")
 print("#           INPUTS ARGS             #")
 print("#-----------------------------------#")
-MODEL_NAME, EMBEDDING_NAME, PDF_FOLDER_PATH, VDB_PATH, COLLECTION_NAME, REUSE_VDB = initfromcmdlineargs()
+MODEL_NAME, EMBEDDING_NAME, IDOC_PATH, VDB_PATH, COLLECTION_NAME, REUSE_VDB, DISPLAY_DOC = initfromcmdlineargs()
 print("MODEL      NAME::", MODEL_NAME)
 print("EMBEDDING  NAME::", EMBEDDING_NAME)
-print("PDF FOLDER PATH::", PDF_FOLDER_PATH)
+print("INPUT DOCS PATH::", IDOC_PATH)
 print("VDB        PATH::", VDB_PATH)
 print("COLLECTION NAME::", COLLECTION_NAME)
 print("REUSE      VDB ::", REUSE_VDB)
-print("PDF FOLDER PATH::", PDF_FOLDER_PATH)
+print("DISPLAY    DOC ::", DISPLAY_DOC)
 print("#-----------------------------------#")
 print("#   STARTING DATA LOAD AND SPLIT    #")
 print("#-----------------------------------#")
-#exit()
+
 splitted_data = None
 if REUSE_VDB is False:
     # Load datas
-    documents = loaddata(PDF_FOLDER_PATH)
+    documents = loaddata(IDOC_PATH)
     print("documents length::", len(documents))
-    for i in range(len(documents)):
-        print(documents[i].page_content[0:300])
+    if DISPLAY_DOC is True:
+        for i in range(len(documents)):
+            print("Printing document ", i, " :")
+            print(documents[i].page_content[0:300])
+    
+    documents = [remove_blankline(d) for d in documents]
+    if DISPLAY_DOC is True:
+        for i in range(len(documents)):
+            print("Printing document after remove_blankline() ", i, " :")
+            print(documents[i].page_content[0:300])
     # Split and chunk
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
+        chunk_size=300,
         chunk_overlap=30,
-        separators=["\n\n", "\n", r"(?<=\. )",  " ", "",]
+        separators=["\n\n", "\n", r"(?<=\. )",  " ", "",
+        "\u200b","\uff0c","\u3001","\uff0e","\u3002",]
         )
     splitted_data = splitter.split_documents(documents)
+    if DISPLAY_DOC is True:
+        for i in range(len(splitted_data)):
+            print("Printing splitted_data ", i, " :")
+            print(splitted_data[i].page_content[0:300])
+    # Split and chunk
 print("#-----------------------------------#")
 print("#     STARTING VECTOR DATABASE      #")
 print("#-----------------------------------#")
@@ -248,7 +315,7 @@ local_model = MODEL_NAME
 llm = ChatOllama(model=local_model, temperature=0)
 QUERY_PROMPT = PromptTemplate(
     input_variables=["question"],
-    template="""You are an AI language model assistant. Your task is to generate two
+    template="""You are an AI language model assistant. Your task is to generate four
     different versions of the given user question to retrieve relevant documents from
     a vector database. By generating multiple perspectives on the user question, your
     goal is to help the user overcome some of the limitations of the distance-based
